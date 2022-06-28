fcbd3b9eb23c7b97fdab7258574439716e3cbe60
aditya <agoswami0708g@gmail.com>
2022-06-28 16:45:52 +0530

    done

--- block-BNaada/sample/routes/user.js
+++ block-BNaada/sample/routes/user.js
@@ -19,7 +19,7 @@ router.post( '/', ( req, res )=>{
 router.get( '/', ( req, res ) => {
     User.find( {}, ( err, user ) =>{
         if ( err ) return next( err );
-        res.render( 'allUsers', { user: user } );
+        res.render( 'allusers', { user: user } );
     } )
 } );
 
